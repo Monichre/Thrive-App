23821bc29afde1de983c1f6cde4362acb6a87ad0
Liam Ellis <liamhellis@gmail.com>
2017-06-24 18:35:11 -0500

    restructing logic and behavior

--- src/App.js
+++ src/App.js
@@ -1,52 +1,174 @@
+// Default Assets
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, IndexRoute, Link, Switch } from 'react-router-dom';
+
+
+// Components
+import Header from './Components/Header.js';
+import Welcome from './Components/Welcome.js';
+import Dashboard from './Components/Dashboard.js';
 import MessageList from './Components/MessageList.js';
 import ChannelList from './Components/ChannelList.js';
-import Header from './Components/Header.js';
 import MessageBox from './Components/MessageBox.js';
+
+// Styles
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+
+// Assets
+import Auth0Lock from 'auth0-lock';
 import Firebase from 'firebase';
 import _ from 'lodash';
 
 
 
 // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyBa7AHgeeKObEk0AoNNi8E3AGE7HVIdo0g",
-//     authDomain: "thrive-app-3a3bc.firebaseapp.com",
-//     databaseURL: "https://thrive-app-3a3bc.firebaseio.com",
-//     projectId: "thrive-app-3a3bc",
-//     storageBucket: "thrive-app-3a3bc.appspot.com",
-//     messagingSenderId: "869272160764"
-// };
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyBa7AHgeeKObEk0AoNNi8E3AGE7HVIdo0g",
+    authDomain: "thrive-app-3a3bc.firebaseapp.com",
+    databaseURL: "https://thrive-app-3a3bc.firebaseio.com",
+    projectId: "thrive-app-3a3bc",
+    storageBucket: "thrive-app-3a3bc.appspot.com",
+    messagingSenderId: "869272160764"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
 
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {},
+            gender: '',
+            website: '',
+            profilePicture: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            userID: '',
+            idToken: '',
+            gender: '',
+            goals: [],
+            loggedIn: false
+        }
+    }
+    static defaultProps = {
+        clientId: "ZsULRd7mfOY8N4dijDo3CmbTto3UDQGW",
+        domain: "monichre.auth0.com"
+    }
+    componentWillMount(){
+        this.lock = new Auth0Lock(this.props.clientId, this.props.domain);
+        this.lock.on('authenticated', (authResult)=> {
+
+            this.lock.getProfile(authResult.idToken, (error, profile) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                console.log(authResult);
+                console.log(profile);
+                this.storeUser(authResult.idToken, profile);
+            });
+        });
+        this.getUser();
+    }
+    storeUser(idToken, profile){
+
+        localStorage.setItem('idToken', idToken);
+        localStorage.setItem('User', JSON.stringify(profile));
+        var newUser = JSON.parse(localStorage.getItem('User'));
+        var userID = newUser.global_client_id;
+
+        this.setState({
+            idToken: localStorage.getItem('idToken'),
+            user: newUser,
+            userID: userID,
+            firstName: newUser.given_name,
+            lastName: newUser.family_name,
+            email: newUser.email,
+            gender: newUser.gender,
+            website: newUser.website,
+            profilePicture: newUser.picture,
+            goals: [],
+            loggedIn: true
+
+        }, function(){
+            console.log(userID);
+        });
+        firebase.database().ref('/users/').set({
+            idToken: localStorage.getItem('idToken'),
+            user: newUser ? newUser : "",
+            userID: userID ? userID : "",
+            firstName: newUser.given_name ? newUser.given_name : "",
+            lastName: newUser.family_name ? newUser.family_name : "",
+            email: newUser.email ? newUser.email : "",
+            gender: newUser.gender ? newUser.gender : "",
+            website: newUser.website ? newUser.website : "",
+            profilePicture: newUser.picture ? newUser.picture : "",
+            goals: [],
+        });
+    }
+    getUser(){
+        if (localStorage.getItem('idToken') != null){
+            var newUser = JSON.parse(localStorage.getItem('User'));
+            this.setState({
+                idToken: localStorage.getItem('idToken'),
+                user: newUser,
+                userID: newUser.global_client_id,
+                firstName: newUser.given_name,
+                lastName: newUser.family_name,
+                email: newUser.email,
+                gender: newUser.gender,
+                website: newUser.website,
+                profilePicture: newUser.picture,
+                goals: [],
+                loggedIn: true
+            }, () => {
+                console.log(this.state);
+            });
+        }
+    }
+    showLock(){
+        this.lock.show();
+    }
+    logOutUser(){
+
+        this.setState({
+            idToken: '',
+            user: '',
+            loggedIn: false
+        }, () => {
+            localStorage.removeItem('idToken');
+            localStorage.removeItem('User');
+        });
     }
 
     render() {
 
-        var style = {
-            margin: '20px'
-        };
+        let page,
+            path;
+
+        if(this.state.loggedIn){
+            page = <Dashboard />;
+        } else {
+            page = <Welcome />;
+        }
+
         return (
             <div>
-                <Header/>
-                <Grid container justify="space-around" gutter={24}>
-                    <Grid item  xs={4}>
-                        <ChannelList />
-                    </Grid>
+                <Header
+                    onLoginClick={this.showLock.bind(this)}
+                    onLogoutClick={this.logOutUser.bind(this)}
+                    loggedIn={this.state.loggedIn}
+                    userName={this.state.firstName}
+                    userGoals={this.state.goals}
+                    lock={this.lock}/>
 
-                    <Grid item  xs={8}>
-                        <MessageList />
-                    </Grid>
-                    <Grid item  xs={12}>
-                        <MessageBox />
-                    </Grid>
+                <Grid container justify="space-around" gutter={24}>
+                    {page}
                 </Grid>
             </div>
         );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
@@ -8,35 +10,55 @@ import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 
-const styleSheet = createStyleSheet('ButtonAppBar', {
-  root: {
-    marginTop: 30,
-    width: '100%',
-  },
-  flex: {
-    flex: 1,
-  },
-});
+class Header extends Component {
 
-function ButtonAppBar(props) {
-  const classes = props.classes;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>Title</Typography>
-          <Button color="contrast">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    constructer(props){
+        super(props);
+    }
+
+    onLoginClick() {
+        alert("Clicked");
+        this.props.onLoginClick();
+    }
+
+    onLogoutClick() {
+        alert("Clicked");
+        this.props.onLogoutClick();
+    }
+
+    render() {
+
+        let authLink,
+            userName;
+        var logo = "img/thrive-logo.png",
+            logoImgStyle = {
+            display: "inline-block"
+        };
+
+        if(this.props.loggedIn){
+            authLink = <li><Link to="" onClick={this.onLogoutClick.bind(this)}>Log Out</Link></li>;
+            userName = <li><a>Welcome {this.props.userName} </a></li>;
+        } else {
+            authLink = <li><Link to="" onClick={this.onLoginClick.bind(this)}>Log In</Link></li>;
+            userName = '';
+        }
+
+        return (
+            <AppBar position="fixed">
+              <Toolbar>
+                <IconButton color="contrast" aria-label="Menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography type="title" color="inherit" className="">Thrive</Typography>
+                <Button>{userName}</Button>
+                <Button color="contrast">{authLink}</Button>
+              </Toolbar>
+            </AppBar>
+        );
+    }
 }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styleSheet)(ButtonAppBar);
+
+
+export default Header;
