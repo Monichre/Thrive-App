57098206f455105722a53cad94fa0b5e6884c178
Liam Ellis <liamhellis@gmail.com>
2017-10-19 00:35:07 -0400

    bugs with ReactBot

--- src/ChatBot/ThriveBot.js
+++ src/ChatBot/ThriveBot.js
@@ -11,13 +11,50 @@ export default class ThriveBot extends Component {
         
         return (
             <ChatBot
-                steps={[
-                {
-                    id: 'hello-world',
-                    message: 'Hello World!',
-                    end: true,
-                },
-                ]}
+                steps= {
+                        [
+                            {
+                                id: '1',
+                                message: 'Im Persis, shall we review your goals?',
+                                
+                                trigger: '2'
+                            },
+                            {
+                                id: '2',
+                                options: [
+                                    { value: 'yes', label: 'Yes?', trigger: 'yes' },
+                                    { value: 'no', label: 'No?', trigger: 'no' }
+                                ]
+                            },
+                            {
+                                id: 'no',
+                                user: true,
+                                trigger: '3'
+                            },
+                            {
+                                id: 'yes',
+                                component: (
+                                    <div> Goal Summary</div>
+                                )
+                            },
+                            {
+                                id: '3',
+                                options: [
+                                    { value: 'yes', label: 'Yes?', trigger: 'newGoal' },
+                                    { value: 'no', label: 'No?', trigger: 'noNewGoal' }
+                                ]
+                            },
+                            {
+                                id: 'newGoal',
+                                message: "New Goal"
+                            },
+                            {
+                                id: 'noNewGoal',
+                                message: "No new Goal",
+                                end: true
+                            }
+                        ]
+                    }
             />
         )
     }
