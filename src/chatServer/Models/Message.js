574d222fb6a3bc240bcdbf3ace11231fd00bc588
Liam Ellis <liamhellis@gmail.com>
2017-12-01 21:32:03 -0500

    git troublefix

--- src/chatServer/Models/Message.js
+++
@@ -1,9 +0,0 @@
-export default {
-    createText: (params, callback) => {
-        const text = {
-            from: params.from,
-            content: params.content
-        }
-        callback(console.log(text))
-    }
-}
